3f881fd343ad1ee75bfb01aa654045d98f66be79
Caio <caio.a@ibm.com>
2023-09-09 02:49:30 -0300

    feat: new page "calculate autonomy"

---
+++ app/src/main/java/br/caioa/electriccar/presentation/CalculateAutonomyActivity.kt
@@ -0,0 +1,50 @@
+package br.caioa.electriccar.presentation
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import br.caioa.electriccar.R
+
+class CalculateAutonomyActivity: AppCompatActivity() {
+    lateinit var price: EditText
+    lateinit var kmTraveled: EditText
+    lateinit var result: TextView
+    lateinit var btnCalculate: Button
+    lateinit var btnClose: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculate_autonomy)
+        setupView()
+        setupListeners()
+    }
+
+    private fun setupView() {
+        price = findViewById(R.id.et_price)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        kmTraveled = findViewById(R.id.et_km_traveled)
+        result = findViewById(R.id.tv_result)
+        btnClose = findViewById(R.id.iv_close)
+    }
+
+    private fun setupListeners() {
+        btnCalculate.setOnClickListener {
+            calculate()
+        }
+
+        btnClose.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun calculate() {
+        val price = price.text.toString().toFloat()
+        val kmTraveled = kmTraveled.text.toString().toFloat()
+
+        val calculatedResult = price / kmTraveled
+        result.text = calculatedResult.toString()
+    }
+}
